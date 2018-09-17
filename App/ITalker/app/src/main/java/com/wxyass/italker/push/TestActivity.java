8ba4970019e2777588002bb0cf89be208e6766a2
wxyass <wxyass@gmail.com>
2018-09-17 14:33:34 +0800

    win和Mac测试

---
+++ App/ITalker/app/src/main/java/com/wxyass/italker/push/TestActivity.java
@@ -0,0 +1,24 @@
+package com.wxyass.italker.push;
+
+import android.widget.TextView;
+
+import net.qiujuer.italker.common.app.Activity;
+
+import butterknife.BindView;
+
+public class TestActivity extends Activity {
+
+    @BindView(R.id.txt_test)
+    TextView mTest;
+
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initWidget() {
+        super.initWidget();
+        mTest.setText("测试");
+    }
+}
